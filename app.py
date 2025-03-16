347390c8133dc57aad584d71bfbf9eef10663c56
sriharshamadamanchi <sriharshamadamanchiz@gmail.com>
2025-03-17 02:12:10 +0530

    add route

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import configparser
 import os
 
 from src.factory import create_app
+app = create_app()
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+@app.route("/")
+def sustains_server():
+    return "<p>Welcome to sustains server!</p>"
+
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run()
