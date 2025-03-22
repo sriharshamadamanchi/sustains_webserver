d9f4d10c5c7ce606846e25f95f6a66f9225e44a1
sriharshamadamanchi <sriharshamadamanchiz@gmail.com>
2025-03-22 12:58:58 +0530

    remove debug

--- src/factory.py
+++ src/factory.py
@@ -20,7 +20,6 @@ def create_app():
     app.register_blueprint(blog_api)
     app.register_blueprint(auth_api)
 
-    app.config['DEBUG'] = True
     app.config['MONGO_URI'] = os.getenv("MONGO_URI")
 
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
